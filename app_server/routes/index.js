d389e88935e2d0b4af268e06e61595413b22df43
Rithik Nair <knrithiknair@gmail.com>
2024-10-22 20:37:11 +0530

    first commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ChatBot' });
+});
+
+router.get('/location', function(req, res, next) {
+  res.render('index', { title: 'Location' });
+});
+
+router.get('/review', function(req, res, next) {
+  res.render('index', { title: 'Review' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'about' });
+});
+
+
+module.exports = router;
